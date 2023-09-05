4a38a0293394e69bd43f1ed0c1aff1239a49042d
AmandaPts <amandatavaresslilas@outlook.com>
2023-09-05 16:03:52 -0300

    Conta Corrente

--- BancoAmbl3/src/br/com/poo/bancoAmbl3/contas/ContaCorrente.java
+++ BancoAmbl3/src/br/com/poo/bancoAmbl3/contas/ContaCorrente.java
@@ -1,5 +1,13 @@
 package br.com.poo.bancoAmbl3.contas;
 
-public class ContaCorrente extends Conta {
+import java.util.logging.Level;
 
+public class ContaCorrente extends Conta {
+	public ContaCorrente(double saldo, String titular){
+
+		super(saldo, titular, numeroConta);
+	}
+	
+	private double limite;
+	
 }
