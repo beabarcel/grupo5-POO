e2bde03aaa384fec3d120ee4d774cf08ffff32a7
LilianeDavid93 <lilianemarquesdavid@gmail.com>
2023-09-13 15:53:00 -0300

    Leitura escrita e resolucao de conflito

--- BancoAmbl3/src/br/com/poo/bancoAmbl3/SistemaInterno.java
+++ BancoAmbl3/src/br/com/poo/bancoAmbl3/SistemaInterno.java
@@ -1,18 +1,23 @@
 package br.com.poo.bancoAmbl3;
 
 import java.io.IOException;
+import java.util.Map;
 
 import br.com.poo.bancoAmbl3.contas.Conta;
+import br.com.poo.bancoAmbl3.enums.TipoRegistro;
 import br.com.poo.bancoAmbl3.io.LeituraEscrita;
 import br.com.poo.bancoAmbl3.pessoas.Cliente;
+import br.com.poo.bancoAmbl3.pessoas.Diretor;
 import br.com.poo.bancoAmbl3.pessoas.Gerente;
 import br.com.poo.bancoAmbl3.pessoas.Presidente;
 
 public class SistemaInterno {
 
 	public static void main(String[] args) throws IOException {
-	
-	
+	  Map<String, Cliente> clientes = new Cliente().buscarClientes();
+	  Map<String, Gerente> gerentes = new Gerente().buscarGerente();
+	  Map<String, Diretor> diretor = new Diretor().buscarDiretor();
+	  Map<String, Presidente> presidente = new Presidente().buscarPresidente();
 	}
 
 }
